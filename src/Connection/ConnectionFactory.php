5fa9886cca60d9b08b4b1923e79edae0f752aa32
chapabu <chapabu101@gmail.com>
2016-01-05 01:09:08 +0000

    Added docblock

--- src/Connection/ConnectionFactory.php
+++ src/Connection/ConnectionFactory.php
@@ -1,5 +1,12 @@
 <?php namespace Searchmetrics\Connection;
 
+/**
+ * Class ConnectionFactory.
+ *
+ * Provides a fully authenticated API connection class.
+ *
+ * @package Searchmetrics\Connection
+ */
 class ConnectionFactory
 {
 
