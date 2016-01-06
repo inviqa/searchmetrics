7bb298a10127f7c4dce99fb0a6e3eed6d995d1fa
chapabu <chapabu101@gmail.com>
2016-01-06 15:48:30 +0000

    Added Guzzl connection and spec.

--- src/Connection/ConnectionFactory.php
+++ src/Connection/ConnectionFactory.php
@@ -7,7 +7,14 @@
  *
  * @package Searchmetrics\Connection
  */
-class ConnectionFactory
+interface ConnectionFactory
 {
 
+    /**
+     * Get the connection instance to the established Searchmetrics session.
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     *   A PSR-7 compatible class instance that represents the connection to Searchmetrics.
+     */
+    public function getClient();
 }
