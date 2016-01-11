398deb27cab377bacd73dc6ac2a641fa13d5dd35
chapabu <chapabu101@gmail.com>
2016-01-11 01:56:41 +0000

    Added Project endpoint spec and implementation

---
+++ src/Factory/Api/ProjectApiFactory.php
@@ -0,0 +1,15 @@
+<?php namespace Searchmetrics\Factory\Api;
+
+/**
+ * Class ProjectApiFactory.
+ *
+ * @package Searchmetrics\Factory\Api
+ */
+class ProjectApiFactory extends ApiFactoryBase implements ApiFactory
+{
+
+    /**
+     * @var string
+     */
+    protected static $baseEndpoint = 'Searchmetrics\\Api\\Project\\';
+}
